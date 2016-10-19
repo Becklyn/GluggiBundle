444220ec6aab7ea53f10746b3a13b708eb059db5
Jannik Zschiesche <jz@becklyn.com>
2016-10-19 14:41:26 +0200

    Added tests

---
+++ tests/Component/ComponentLoaderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Becklyn\GluggiBundle\Component;
+
+use Becklyn\GluggiBundle\Component\ComponentLoader;
+use Becklyn\GluggiBundle\Data\ComponentType;
+
+
+/**
+ *
+ */
+class ComponentLoaderTest extends \PHPUnit_Framework_TestCase
+{
+    private function loadAtoms ()
+    {
+        $loader = new ComponentLoader(__DIR__ . "/../fixtures/Resources/views");
+        $type = self::getMockBuilder(ComponentType::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $type
+            ->method("getDirectory")
+            ->willReturn("atom");
+
+        return $loader->loadComponents($type);
+    }
+
+
+
+    public function testViewDirectory ()
+    {
+        $components = $this->loadAtoms();
+
+        self::assertArrayHasKey("_hidden", $components);
+        self::assertArrayHasKey("correct", $components);
+        self::assertCount(2, $components);
+    }
+}
---
+++ tests/Data/ComponentTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Becklyn\GluggiBundle\Data;
+
+use Becklyn\GluggiBundle\Data\Component;
+use Becklyn\GluggiBundle\Data\ComponentType;
+
+
+class ComponentTest extends \PHPUnit_Framework_TestCase
+{
+    private function getInstance (string $fileName)
+    {
+        $type = self::getMockBuilder(ComponentType::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $type
+            ->method("getDirectory")
+            ->willReturn("atom");
+
+        return new Component(new \SplFileInfo(__DIR__ . "/../fixtures/Resources/views/atom/{$fileName}"), $type);
+    }
+
+
+
+    public function testBasicData ()
+    {
+        $component = $this->getInstance("_hidden.html.twig");
+        self::assertSame("_hidden", $component->getKey());
+        self::assertSame("Hidden", $component->getName());
+        self::assertTrue($component->isHidden());
+    }
+}
