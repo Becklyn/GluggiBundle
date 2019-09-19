490b4fb131538ade6cd4464cf3039d9ceeaa5302
Jannik Zschiesche <jz@becklyn.com>
2019-09-19 18:24:35 +0200

    Change sidebar handling + store state

--- assets/js/handlers/sidebar.ts
+++ assets/js/handlers/sidebar.ts
@@ -1,11 +1,12 @@
 import {on} from "mojave/dom/events";
-import {createUnstyledElement, prepend, toggleClass} from "mojave/dom/manipulate";
+import {createUnstyledElement, prepend} from "mojave/dom/manipulate";
 import {findOne} from "mojave/dom/traverse";
 
 // @ts-ignore
 import closeIcon from "../../icon/close.svg";
 // @ts-ignore
 import menuIcon from "../../icon/menu.svg";
+
 
 /**
  * Initializes the close button
@@ -19,14 +20,32 @@ export function initSidebarVisibilityToggle (container: HTMLElement)
 
     let button = createUnstyledElement("button", {
         class: "gluggi-sidebar-toggle",
-        html: `<span class="gluggi-default">${menuIcon}</span><span class="gluggi-toggled">${closeIcon}</span>`,
+        html: `<span class="gluggi-icon-menu">${menuIcon}</span><span class="gluggi-icon-close">${closeIcon}</span>`,
     });
 
-    let isToggled = false;
     prepend(container, button);
+    let classes = container.classList;
+    let opened = "open" === window.localStorage.getItem("gluggi:sidebar");
+
+    let open = () => {
+        classes.add("gluggi-is-open");
+        opened = true;
+        window.localStorage.setItem("gluggi:sidebar", "open");
+    };
+
+    let close = () => {
+        classes.remove("gluggi-is-open");
+        opened = false;
+        window.localStorage.removeItem("gluggi:sidebar");
+    };
 
     on(button, "click", () => {
-        isToggled = !isToggled;
-        toggleClass(container, "gluggi-is-toggled", isToggled);
+        (opened ? close : open)();
     });
+
+    // if initially opened -> open
+    if (opened)
+    {
+        open();
+    }
 }
