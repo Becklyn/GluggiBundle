70a63a22383d10c026781a200d299b978033836a
Kai Eichinger <ke@becklyn.com>
2020-07-27 13:39:17 +0200

    Remove unused import

--- tests/Controller/GluggiControllerTest.php
+++ tests/Controller/GluggiControllerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Becklyn\GluggiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class GluggiControllerTest extends WebTestCase
 {
